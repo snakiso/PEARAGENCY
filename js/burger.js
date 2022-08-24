584df4b3900db56ba8ca9c02f012535a2559fb6d
igorchagov <igorchagov@mail.ru>
2022-08-24 17:40:51 +0300

    add close nav

--- js/burger.js
+++ js/burger.js
@@ -6,5 +6,15 @@ hamburger.addEventListener('click', () => {
  nav.classList.toggle('active')
 })
 
+ $(document).mouseup(function (e) { // событие клика по веб-документу
+  var div = $(".nav-mobile"); // Элемент, клик по которому не должен приводить к закрытию. 
+  if (!div.is(e.target) // если клик был не по нашему блоку
+   && div.has(e.target).length === 0) { // и не по его дочерним элементам
+   $('.menu-open').removeClass('active');
+   $('.nav-mobile').removeClass('active');
+  }
+ });
+
+
 var $containers = $('[data-animation]:not([data-animation-child]), [data-animation-container]');
 $containers.scrollAnimations();
