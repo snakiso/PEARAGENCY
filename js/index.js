6c6539a50ae61fc453f627a3a9c141ce8627355d
igorchagov <igorchagov@mail.ru>
2022-08-25 11:51:50 +0300

    added parallax

--- js/index.js
+++ js/index.js
@@ -10,3 +10,9 @@ var swiper = new Swiper('.reviews-slider', {
   clickable: true
  }
 });
+
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene, {
+ relativeInput: true
+});
+parallaxInstance.friction(0.2, 0.2)
