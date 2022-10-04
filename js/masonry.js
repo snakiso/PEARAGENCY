02aacdb5d4d67a653678d5cf53301c071e2ebf56
igorchagov <igorchagov@mail.ru>
2022-10-04 12:45:11 +0300

    fixed  blog

--- js/masonry.js
+++ js/masonry.js
@@ -1,7 +1,10 @@
-var grid = document.querySelector('.grid');
-var msnry = new Masonry(grid, {
- // options
- itemSelector: '.grid-item',
- //columnWidth: 286,
- gutter: 30
-});
+window.addEventListener('load', () => {
+ var grid = document.querySelector('.grid');
+ var msnry = new Masonry(grid, {
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 286,
+  gutter: 30,
+  resize: true,
+ });
+})
